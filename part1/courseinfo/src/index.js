180cb402ba5ee8b8db50f16eead2c8b5054922da
federikos <federikos@bk.ru>
2019-08-29 20:15:32 +0300

    complete exercise #1.4 Course info, step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -9,12 +9,16 @@ const Part = ({part}) => (
     <p>{part.name} {part.exercises}</p>
 )
 
-const Content = ({parts}) => {
+const Content = (props) => {
+  const parts = props.parts;
+
   return (
     <div>
-      <Part part={parts[0]}/>
-      <Part part={parts[1]}/>
-      <Part part={parts[2]}/>
+      {
+        parts.map(part => (
+          <Part part={part}/>
+        ))
+      }
     </div>
   )
 }
@@ -25,26 +29,26 @@ const Total = ({parts}) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content 
-        parts={[part1, part2, part3]} 
-      />
-      <Total parts={[part1, part2, part3]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
