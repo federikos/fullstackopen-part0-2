cc8b83a20ba8cb2e53753f6edd29b61693a0d507
federikos <federikos@bk.ru>
2019-08-29 16:55:59 +0300

    complete exercise #1.1 Course info, step 1

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({course}) => (
+  <h1>{course}</h1>
+)
+
+const Content = ({paragraphs}) => (
+    paragraphs.map(paragraph => {
+      return <p>{paragraph.part} {paragraph.exercises}</p>
+    })
+)
+
+const Total = ({exercises}) => (
+  <p>Number of exercises {exercises.reduce((a, b) => a + b)}</p>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content 
+        paragraphs={[
+          {part: part1, exercises: exercises1},
+          {part: part2, exercises: exercises2},
+          {part: part3, exercises: exercises3}
+        ]} 
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
