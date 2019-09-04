941fd776bc639973e2cf4ab0df9283a43a1fd110
federikos <federikos@bk.ru>
2019-09-04 16:12:51 +0300

    complete exercise #2.6 The Phonebook Step1

---
+++ part2/the_phonebook/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const Persons = ({ persons }) => (
+  <div>
+  { persons.map(person => <p key={person.name}>{person.name}</p>) }
+  </div>
+)
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({name: newName}));
+    setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons}/>
+    </div>
+  )
+}
+
+export default App
