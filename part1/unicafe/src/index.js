47f1f7a2457b9ee3ffde22d83e869aa7576fb4f1
federikos <federikos@bk.ru>
2020-05-19 02:10:49 +0300

    combine several state instances into one, shorten function handleBtnClick

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,25 +3,24 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [ feedback, setFeedback ] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
 
-  const handleBtnClick = (label) => () => {
-    switch (label) {
-      case 'good':
-        setGood(good + 1);
-        break;
-      case 'neutral':
-        setNeutral(neutral + 1);
-        break;
-      case 'bad':
-        setBad(bad + 1);
-        break;
-      default:
-        console.error(`There are no function for label "${label}"`);     
+  const handleBtnClick = label => () => {
+    if (!Object.keys(feedback).includes(label)) {
+      console.error(`There are no state for label "${label}"`);
+      return;
     }
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [label]: prevFeedback[label] + 1
+    }))
   }
+
+  const { good, neutral, bad } = feedback;
 
   return (
     <div>
