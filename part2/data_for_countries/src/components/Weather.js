f24a4a2840fa5dc0735b21c7a50fd3529423fca5
federikos <federikos@bk.ru>
2020-05-19 23:19:19 +0300

    add weather icon

--- part2/data_for_countries/src/components/Weather.js
+++ part2/data_for_countries/src/components/Weather.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Weather = ({ weatherData, country }) => {
   if (!weatherData) return null;
+  const { clouds: { all }, wind: { speed, deg }, main: { temp }, weather } = weatherData;
+  const { icon: weatherIcon, description: weatherDescription } = weather[0];
   return (
     <div>
       <h3>Weather in {country.capital}</h3>
-      <p><b>temperature:</b> {weatherData.main.temp} Celsius</p>
-      <p><b>overcast:</b> {weatherData.clouds.all}%</p>
-      <p><b>wind:</b> {weatherData.wind.speed} kph, direction {weatherData.wind.deg} degree</p>
+      <img src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`} alt={weatherDescription}/>
+      <p><b>temperature:</b> {temp} Celsius</p>
+      <p><b>overcast:</b> {all}%</p>
+      <p><b>wind:</b> {speed} kph, direction {deg} degree</p>
     </div>
   )
 }
